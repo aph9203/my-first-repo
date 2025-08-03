d6de420f239d34fa6ed311e3a985335eb979b94c
Akanksha Hansdah <aph9203@nyu.edu>
2025-08-03 19:41:38 -0400

    Ran tests and fixed the bug

--- app/rps.py
+++ app/rps.py
@@ -1,32 +1,41 @@
 # This is the rock paper siccors game
 
-print("Welcome to the game")
-
-player_choice = input("Please input an option: ('rock', 'paper', 'scissors')")
-print("USER CHOICE:", player_choice)
-
 import random
+
 VALID_OPTIONS = ["rock", "paper", "scissors"]
 
-computer_choice = random.choice(VALID_OPTIONS)
-print("COMPUTER CHOICE:", computer_choice)
+# here the input variable can be named any other way not necessarily the one used for input 
+def determine_winner(player_choice, computer_choice):
+        if player_choice == computer_choice:
+            result = "TIE GAME"
+        elif player_choice == "rock" and computer_choice == "scissors":
+            result = "USER WINS"
+        elif player_choice == "rock" and computer_choice == "paper":
+            result = "COMP WINS"
+        elif player_choice == "scissors" and computer_choice == "rock":
+            result = "COMP WINS"
+        elif player_choice == "scissors" and computer_choice == "paper":
+            result = "USER WINS"
+        elif player_choice == "paper" and computer_choice == "rock":
+            # result = "COMP WINS" this was a bug
+            result = "USER WINS"
+        elif player_choice == "paper" and computer_choice == "scissors":
+            result = "COMP WINS"
+        return result
 
-def determine_winner(player_choice1, computer_choice1):
-    if player_choice == computer_choice:
-        result = "TIE GAME"
-    elif player_choice == "rock" and computer_choice == "scissors":
-        result = "USER WINS"
-    elif player_choice == "rock" and computer_choice == "paper":
-        result = "COMP WINS"
-    elif player_choice == "scissors" and computer_choice == "rock":
-        result = "COMP WINS"
-    elif player_choice == "scissors" and computer_choice == "paper":
-        result = "USER WINS"
-    elif player_choice == "paper" and computer_choice == "rock":
-        result = "COMP WINS"
-    elif player_choice == "paper" and computer_choice == "scissors":
-        result = "COMP WINS"
-    return result
+# ONLY RUN THE CODE INDENTED INSIDE
+# ... IF WE ARE RUNNING THIS SCRIPT FROM THE COMMAND LINE
+# ... BUT NOT IF WE ARE IMPORTING
 
-result_message = determine_winner(player_choice, computer_choice)
-print(result)
+if __name__ == "__main__":
+
+    #player_choice = input("Please input an option: ('rock', 'paper', 'scissors')")
+    print("Welcome to the game")
+    player_choice = input("Please input an option: ('rock', 'paper', 'scissors')")
+    print("USER CHOICE:", player_choice)
+    computer_choice = random.choice(VALID_OPTIONS)
+    print("COMPUTER CHOICE:", computer_choice)
+
+    result_message = determine_winner(player_choice, computer_choice)
+    print(result_message)
+
