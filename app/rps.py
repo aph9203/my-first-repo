de6708aa89f1bc211412d024d3fbc4ea928dbba4
Akanksha Hansdah <aph9203@nyu.edu>
2025-08-03 18:36:23 -0400

    Update rps.py

--- app/rps.py
+++ app/rps.py
@@ -11,20 +11,22 @@ VALID_OPTIONS = ["rock", "paper", "scissors"]
 computer_choice = random.choice(VALID_OPTIONS)
 print("COMPUTER CHOICE:", computer_choice)
 
-if player_choice == computer_choice:
-    result = "TIE GAME"
-elif player_choice == "rock" and computer_choice == "scissors":
-    result = "USER WINS"
-elif player_choice == "rock" and computer_choice == "paper":
-    result = "COMP WINS"
-elif player_choice == "scissors" and computer_choice == "rock":
-    result = "COMP WINS"
-elif player_choice == "scissors" and computer_choice == "paper":
-    result = "USER WINS"
-elif player_choice == "paper" and computer_choice == "rock":
-    result = "COMP WINS"
-elif player_choice == "paper" and computer_choice == "scissors":
-    result = "COMP WINS"
+def determine_winner(player_choice1, computer_choice1):
+    if player_choice == computer_choice:
+        result = "TIE GAME"
+    elif player_choice == "rock" and computer_choice == "scissors":
+        result = "USER WINS"
+    elif player_choice == "rock" and computer_choice == "paper":
+        result = "COMP WINS"
+    elif player_choice == "scissors" and computer_choice == "rock":
+        result = "COMP WINS"
+    elif player_choice == "scissors" and computer_choice == "paper":
+        result = "USER WINS"
+    elif player_choice == "paper" and computer_choice == "rock":
+        result = "COMP WINS"
+    elif player_choice == "paper" and computer_choice == "scissors":
+        result = "COMP WINS"
+    return result
 
+result_message = determine_winner(player_choice, computer_choice)
 print(result)
-
